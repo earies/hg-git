earies/hg-git

commit 032079b119885ec7e36fa5dedab0108f335e81a5

ssh: fix breakage with dulwich update
dulwich 0.11.x changed the way they pass parameters around, so we reformat that
to what hg-git expects.

hggit/_ssh.py
@@ -14,6 +14,11 @@ def generate_ssh_vendor(ui):
 
     class _Vendor(SSHVendor):
         def run_command(self, host, command, username=None, port=None):
+            # newer dulwich changes the way they pass command and parameters
+            # around, so we detect that here and reformat it back to what
+            # hg-git expects (e.g. "command 'arg1 arg2'")
+            if len(command) > 1:
+                command = ["%s '%s'" % (command[0], ' '.join(command[1:]))]
             sshcmd = ui.config("ui", "ssh", "ssh")
             args = util.sshargs(sshcmd, host, username, port)
             cmd = '%s %s %s' % (sshcmd, args,

commit 3c4ff461d0a80e555db07369757b40cb15d62c6a

ssh: fix arg issue with dulwich 0.12.x
It appears commit 032079b1 introduced a method to structure arguments in
such a way to be compatible with dulwich 0.11.x.  This however breaks in
0.12.x versions.  Reverting the above commit and restructuring the join
in the git command passed to ssh.

hggit/_ssh.py
@@ -14,15 +14,10 @@ def generate_ssh_vendor(ui):
 
     class _Vendor(SSHVendor):
         def run_command(self, host, command, username=None, port=None):
-            # newer dulwich changes the way they pass command and parameters
-            # around, so we detect that here and reformat it back to what
-            # hg-git expects (e.g. "command 'arg1 arg2'")
-            if len(command) > 1:
-                command = ["%s '%s'" % (command[0], ' '.join(command[1:]))]
             sshcmd = ui.config("ui", "ssh", "ssh")
             args = util.sshargs(sshcmd, host, username, port)
             cmd = '%s %s %s' % (sshcmd, args,
-                                util.shellquote(' '.join(command)))
+                                util.shellquote(''.join(command)))
             ui.debug('calling ssh: %s\n' % cmd)
             proc = subprocess.Popen(util.quotecommand(cmd), shell=True,
                                     stdin=subprocess.PIPE,